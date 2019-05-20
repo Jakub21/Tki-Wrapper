cc957ed75ddc7aeaaabeaa230818c0d3ae76a7bc
Jakub21 <jakub21@mail.com>
2019-05-20 18:04:36 +0200

    Create sub-package canvas

--- TkiWrapper/__init__.py
+++ TkiWrapper/__init__.py
@@ -3,5 +3,4 @@ from TkiWrapper.viewMain import View
 from TkiWrapper.viewDialog import Dialog
 from TkiWrapper.style import Style
 from TkiWrapper.grid import Grid
-from TkiWrapper.canvas import Canvas
 from TkiWrapper.namespace import Namespace
---
+++ TkiWrapper/canvas/__init__.py
@@ -0,0 +1,4 @@
+from TkiWrapper.canvas.canvas import Canvas
+from TkiWrapper.canvas.point import Point
+from TkiWrapper.canvas.image import Image
+from TkiWrapper.canvas.element import *
