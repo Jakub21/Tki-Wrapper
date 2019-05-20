bf1ae4f4f0f59965d6f813af9d5aea4d8f5d8431
Jakub21 <jakub21@mail.com>
2019-05-20 18:04:15 +0200

    Add sub-package auto detection to setup.py
    Update version

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
     long_description = f.read()
@@ -7,9 +7,9 @@ setup(
     name = 'TkiWrapper',
     description = 'Create grid-based GUIs faster and easier',
     long_description = long_description,
-    version = '0.3',
+    version = '0.5',
     license = 'MIT',
     author = 'Jakub21',
     url = 'https://github.com/Jakub21',
-    packages = ['TkiWrapper'],
+    packages = find_packages(),
 )
